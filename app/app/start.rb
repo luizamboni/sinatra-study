90f29bb01cf2a8ef092033c5045122f6badb47da
Luiz Zamboni <luizamboni2002@hotmail.com>
2026-01-22 20:19:57 -0300

    Remove seed flow

--- app/app/start.rb
+++ app/app/start.rb
@@ -4,27 +4,21 @@
 require "sorbet-runtime"
 require_relative "../app"
 require_relative "dependency_builder"
-require_relative "./seed"
 
 module App::App
   extend T::Sig
 
-  sig { returns(App::App::DependencyBuilder::Container) }
+
+  sig { returns(DependencyBuilder::Container) }
   def self.build
     DependencyBuilder.build(
-      repository_class: App::Infrastructure::Repository
+      repository_class: ::App::Infrastructure::Repository
     )
   end
 
   # Sorbet signatures document and check method return types.
-  sig { returns(T::Hash[Symbol, T::Array[App::Domain::Entity]]) }
+  sig { returns(DependencyBuilder::Container) }
   def self.start
-    container = self.build
-    schema_names = Seed.run(container: container)
-
-    {
-      users: container.dynamic_entity_service.entities_for(schema_name: T.must(schema_names[:users])),
-      posts: container.dynamic_entity_service.entities_for(schema_name: T.must(schema_names[:posts]))
-    }
+    self.build
   end
 end
